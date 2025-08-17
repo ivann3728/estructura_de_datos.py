6b3ad44c5445f03057ba5d2caf4270f1d09105b1
Ivan <ivan_aguilar@hotmail.com.ar>
2025-08-17 01:08:12 -0300

    Realizamos un breve resumen de la estructura de datos en Python.

---
+++ estructura_de_datos.py
@@ -0,0 +1,32 @@
+
+# TIPOS DE DATOS SECUENCIA / COLECCION
+
+# - list (lista) [Colección ordenada y mutable]
+
+lista = [1,2,3,4]
+
+# - tuple (tupla) [Colección ordenada e inmutable]
+
+tupla = (1,2,3,4)
+
+# - range (rango) [Secuencia inmutable de números]
+
+rango = range (0,10)
+
+# TIPOS DE DATOS DE MAPEO (MAPPING TYPE)
+
+# dict (diccionario) [Colección no ordenada de pares clave - valor]
+
+diccionario = {"nombre": "Sergie Code", "edad": 34,"tecnologías": ["Python", "JavaScript"]}
+
+# TIPOS DE DATOS CONJUNTOS
+
+# - set (conjunto) [Colección no ordenada y mutable de elementos únicos]
+
+conjunto = {1,2,3,4}
+
+# TIPO DE DATO NULO
+
+# - NoneType (nulo) [Representa la ausencia de valor o la no defición del mismo]
+
+nulo = None
